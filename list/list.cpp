4047462de4b815462a99bfffda746ee5c27d9d27
Kamonthip Suwankoet <kamonthip_s@cmu.ac.th>
2024-07-01 12:42:44 +0700

    finish

--- list/list.cpp
+++ list/list.cpp
@@ -19,9 +19,18 @@ void List::pushToHead(char el)
 		tail = head;
 	}
 }
-void List::pushToTail(char el)
+void List::pushToTail(char add_tail)
 {
 	//TO DO!
+	Node* p = new Node(add_tail);
+	if (head == NULL) {
+		head = p;
+		tail = p;
+	}
+	else {
+		tail->next = p;
+		tail = p;
+	}
 }
 char List::popHead()
 {
@@ -41,16 +50,41 @@ char List::popHead()
 char List::popTail()
 {
 	// TO DO!
-	return NULL;
+	Node* p = head;
+	while (p->next != tail) {
+		p = p->next;
+	}
+	delete tail;
+	p->next = 0;
+	tail = p;
+	return 0;
 }
 bool List::search(char el)
 {
 	// TO DO! (Function to return True or False depending if a character is in the list.
-	return NULL;
+	Node* p = head;
+	while (p->next != NULL) {
+		if (p->data == el) {
+			cout << "True" << endl;
+			return true;
+		}
+		else {
+			p = p->next;
+		}
+	}
+	cout << "False" << endl;
+	return false;
 }
 void List::reverse()
 {
 	// TO DO! (Function is to reverse the order of elements in the list.
+		List rev;
+	for (Node* p = head; p != 0; p = p->next)
+	{
+		char el = p->data;
+		rev.pushToHead(el);
+	}
+	rev.print();
 }
 void List::print()
 {
--- list/program.cpp
+++ list/program.cpp
@@ -7,10 +7,16 @@ void main()
 	//Sample Code
 	List mylist;
 	mylist.pushToHead('i');
-	mylist.pushToHead('s');
+	mylist.pushToHead('i');
 	mylist.pushToHead('n');
-	mylist.pushToHead('e');
+	mylist.pushToHead('g');
+	mylist.pushToHead('g');
+	mylist.popHead();
+	mylist.popTail();
+	mylist.reverse();
 	mylist.print();
+	cout << endl;
+	mylist.search('n');
 
 	//TO DO! Write a program that tests your list library - the code should take characters, push them onto a list, 
 	//- then reverse the list to see if it is a palindrome!
